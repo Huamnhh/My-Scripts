ee69ecd436e8d4a3960bafc840b62638d2b92194
Huamnhh <67453106+Huamnhh@users.noreply.github.com>
2025-04-29 21:34:32 +0800

    Update Excluded_list.js

--- Excluded_list.js
+++ Excluded_list.js
@@ -403,4 +403,20 @@ const EXCEL_COMPANIES = [
 
   //没找到的公司
   "Andeltech Electronics, GIGAHARDWARE",
+
+  //中国公司
+  "Acorn Tech Service Limited",
+  "Quiksol International HK PTE limiteds  ",
+  "Highsource Technology Co.Ltd",
+  "CENTURY FUGUANG CO., LTD.",
+  "Grandtec Industrial (Hongkong) Co., Ltd",
+  "Shengyue Technology Co., Ltd",
+  "Convey Telecom Limited",
+  "Top IT Service Limited,fka Uniworld",
+  "DECHO Network Technology LTD",
+  "Joy Online Network Limited",
+  "Coopak Networks",
+
+  //被拉黑
+  "Memory Clearance",
 ];
