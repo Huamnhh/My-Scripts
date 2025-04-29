224cbd07fcce0bc8752ede7a3ab645a1b3af8fbf
Huamnhh <67453106+Huamnhh@users.noreply.github.com>
2025-04-29 21:08:04 +0800

    Add files via upload

---
+++ Secondary_list.js
@@ -0,0 +1,16 @@
+const EXCEL_COMPANIES_2 = [
+  "Acorn Tech Service Limited",
+  "Quiksol International HK PTE limiteds  ",
+  "Highsource Technology Co.Ltd",
+  "CENTURY FUGUANG CO., LTD.",
+  "Grandtec Industrial (Hongkong) Co., Ltd",
+  "Shengyue Technology Co., Ltd",
+  "Convey Telecom Limited",
+  "Top IT Service Limited,fka Uniworld",
+  "DECHO Network Technology LTD",
+  "Joy Online Network Limited",
+  "Coopak Networks",
+
+//被拉黑
+  "Memory Clearance",
+];
